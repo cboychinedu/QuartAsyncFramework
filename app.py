1e20dbc2a2827401f77aa38045819979ea76f7c4
Mbonu Chinedum <cboy.chinedu@gmail.com>
2023-08-22 22:18:33 +0100

    logging

--- app.py
+++ app.py
@@ -15,11 +15,16 @@ from About.routes import about
 app = Quart(__name__) 
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-app.secret_key = "kdsd8*DEKFE!@$#$$REDKDNI*(KEHE&E&^^$D"
+app.secret_key = "kdsd8*DEKFE!@$#$$REDKDNI*(KEHE&E&^"
 app.permanent_session_lifetime = timedelta(days=24)
 
 # Setting the cors application 
 app = cors(app, allow_origin="*")
+
+# # Logging the configurations to a file on disk 
+logging.basicConfig(filename="Logs/requests.log", level=logging.DEBUG, 
+                    format="%(asctime)s %(message)s %(filename)s %(module)s %(pathname)s", 
+                    datefmt="%m/%d/%Y %I:%M:%S %p")
 
 # Register the views using the "app.register" function 
 app.register_blueprint(home, url_prefix="/")
